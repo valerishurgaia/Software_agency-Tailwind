550763e8a1302edb67b6dd35ee781be36d546eef
valerishurgaia <valerishurgo@gmail.com>
2023-01-31 00:10:30 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'pinkTree': "url('./img/lateNight.jpg')",
+                'Square-blue': "url(./img/Square-blue.jpg)",
+                'Blue-Nature': "url(./img/Blue-Nature.jpg)",
+                'Neon-Blue': "url(./img/Neon-Blue.jpg)"
+                },
+                height: {
+                    '800': '50rem',
+                    '740': '46.25rem',
+                    '485': '30.25rem',
+                },
+                colors: {
+                    'dark-blue': '#0C1741',
+                    'almost-grey':'#555151',
+                    'light-blue': "#4A72FF",
+                    'light-black': "#373636"
+                },
+                width: {
+                    '600': '40rem',
+                    '688': '44rem'
+                }
+        },
+    },
+    plugins: [],
+}
